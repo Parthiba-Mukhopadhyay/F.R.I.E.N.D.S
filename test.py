7fb32c965c8f2ab8e4c3723d913f2ffe53112935
srijena <srijenaguin@gmail.com>
2023-04-03 00:48:37 +0530

    srijena_update

--- test.py
+++ test.py
@@ -10,26 +10,29 @@ df.dropna(how='all',axis=1,inplace=True)
 
 
 #reading the user input
-pettype=st.selectbox('type of pet:',['Choose your own option']+list(df['PET'].unique()))
-breed=st.selectbox('breed:',['Choose your own option']+list(df[df['PET']==pettype]['BREED'].unique()))
-age=st.selectbox('age of your pet:',['Choose your own option']+list(df[df['BREED']==breed]['AGE']))
-gender=st.selectbox('gender of pet:',['Choose your own option']+list(df['GENDER'].unique()))
+pettype=st.selectbox('type of pet:',list(df['PET'].unique()))
+breed=st.selectbox('breed:',list(df[df['PET']==pettype]['BREED'].unique()))
+age=st.selectbox('age of your pet:',list(df[df['BREED']==breed]['AGE']))
+dfr=df[df['BREED']==breed][['GENDER','AGE']]
+gender=st.selectbox('gender of pet:',list(dfr[dfr['AGE']==age]['GENDER'].unique()))
 expense=st.slider(label='expense per month',min_value=2000,max_value=30000,step=100)
 
 #extracting expense from the dataset 
 dfr=df[df['BREED']==breed][['GENDER','EXPENSES','AGE']]
 dfrr=dfr[dfr['AGE']==age][['GENDER','EXPENSES']]
-exp=dfrr[dfrr['GENDER']==gender]['EXPENSES']
-
-st.text(exp)
-
-'''
+exp=dfrr[dfrr['GENDER']==gender]['EXPENSES'].tolist()
+expf=exp[0]
+#st.text(expf)
 #comparing the required expense and the amount the user has specified
-if(expense<exp):
-  amount=exp-expense
-  st.text_area(label='',placeholder='You need to invest'+str(amount)+'more \n No compatibility, please allocate more responses for the maintenance of the pet')
-  
-else:
-  st.text_area(label='',placeholder='Compatible, you may go ahead with the adoption!')
-'''
+def compare():
+    if(expense<expf):
+        amount=expf-expense
+        st.text_area(label='Results: ',placeholder='You need to invest '+str(amount)+' more \n No compatibility, please allocate more responses for the maintenance of the pet')
+    
+    else:
+        st.text_area(label='Results: ',placeholder='Compatible, you may go ahead with the adoption!')
+
+if st.button(label='compare',type='primary',disabled=False,use_container_width=True):
+    compare()
+
 
