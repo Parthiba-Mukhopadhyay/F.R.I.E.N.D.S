870a8a03fd3f0c380cffcf52f1950b2fb9e8167f
0sigma101 <pallav.gupta2021@vitstudent.ac.in>
2023-04-03 00:53:27 +0530

    Compatibility check

--- compatibility.py
+++ compatibility.py
@@ -7,50 +7,32 @@ df=pd.read_csv('./content./dataset.csv')
 #dropping null values
 df.dropna(how='all',axis=1,inplace=True)
 
-#converting file to csv
-df.to_csv('dataset.csv',index=False)
+
 
 #reading the user input
-dfr=pd.read_csv('./content/responses_schallenge.csv')
+pettype=st.selectbox('type of pet:',list(df['PET'].unique()))
+breed=st.selectbox('breed:',list(df[df['PET']==pettype]['BREED'].unique()))
+age=st.selectbox('age of your pet:',list(df[df['BREED']==breed]['AGE']))
+dfr=df[df['BREED']==breed][['GENDER','AGE']]
+gender=st.selectbox('gender of pet:',list(dfr[dfr['AGE']==age]['GENDER'].unique()))
+expense=st.slider(label='expense per month',min_value=2000,max_value=30000,step=100)
 
-#converting to csv file
-dfr.to_csv('responses_schallenge.csv')
-
-#using csv module, read both the files
-filef=open('dataset.csv','r')
-filer=open('responses_schallenge.csv','r')
-
-csvrf=csv.reader(filef)
-csvrr=csv.reader(filer)
-
-#extracting data from the dataset 
-rowf=[]
-for row in csvrf:
-  rowf.append(row)
-
-#extracting data from the user
-rowr=[]
-for row in csvrr:
-  rowr.append(row)
-
-expf = 0
-expr = 0
-#comparing the data from the dataset with the one given by the user
-for x in rowf:
-  for y in rowr:
-    if x[1]==y[3]:
-        expf=x[5]
-        expr=y[5]
-
-#converting string values to float
-conv_expf=float(expf)
-conv_expr=float(expr)
-
+#extracting expense from the dataset 
+dfr=df[df['BREED']==breed][['GENDER','EXPENSES','AGE']]
+dfrr=dfr[dfr['AGE']==age][['GENDER','EXPENSES']]
+exp=dfrr[dfrr['GENDER']==gender]['EXPENSES'].tolist()
+expf=exp[0]
+#st.text(expf)
 #comparing the required expense and the amount the user has specified
-if(conv_expr<(0.5*conv_expf)):
-  amount=conv_expf-conv_expr
-  st.text_area(label='',placeholder='you need to invest more \nno compatibility, please allocate more responses for the maintenance of the pet')
-  
-else:
-  st.text_area(label='',placeholder='compatible, you may go ahead with the adoption!')
+def compare():
+    if(expense<expf):
+        amount=expf-expense
+        st.text_area(label='Results: ',placeholder='You need to invest '+str(amount)+' more \n No compatibility, please allocate more responses for the maintenance of the pet')
+    
+    else:
+        st.text_area(label='Results: ',placeholder='Compatible, you may go ahead with the adoption!')
+
+if st.button(label='compare',type='primary',disabled=False,use_container_width=True):
+    compare()
+
 
